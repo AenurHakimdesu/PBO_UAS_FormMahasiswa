1405ed9b91cecef2b73d4a3f1b6ca36bd79974af
AenurHakimdesu <147078743+AenurHakimdesu@users.noreply.github.com>
2024-07-07 16:44:14 +0700

    Upload formPendaftaranMahasiswa-Release-v1
    Perilisan formPendaftaranMahasiswa v1

---
+++ formMahasiswa-Release-v1/src/Models/ModelDataLengkap.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Hikma
+ */
+public class ModelDataLengkap {
+    private int ID_Siswa;
+    private String Nama_Siswa;
+    private String Tgl_Lahir;
+    private String Asal_Sekolah;
+    private String Jenis_Kelamin;
+    private String Kota_Asal;
+    private String Email_Siswa;
+    private int ID_Wali;
+    private String Nama_Wali;
+    private int ID_Prodi;
+    private String Nama_Prodi;
+    
+    public ModelDataLengkap(int ID_Siswa, String Nama_Siswa, String Tgl_Lahir, String Asal_Sekolah, String Jenis_Kelamin, String Kota_Asal, String Email_Siswa, int ID_Wali, String Nama_Wali, int ID_Prodi, String Nama_Prodi) {
+        this.ID_Siswa = ID_Siswa;
+        this.Nama_Siswa = Nama_Siswa;
+        this.Tgl_Lahir = Tgl_Lahir;
+        this.Asal_Sekolah = Asal_Sekolah;
+        this.Jenis_Kelamin = Jenis_Kelamin;
+        this.Kota_Asal = Kota_Asal;
+        this.Email_Siswa = Email_Siswa;
+        this.ID_Wali = ID_Wali;
+        this.Nama_Wali = Nama_Wali;
+        this.ID_Prodi = ID_Prodi;
+        this.Nama_Prodi = Nama_Prodi;
+
+    }
+
+    /**
+     * @return the ID_Siswa
+     */
+    public int getID_Siswa() {
+        return ID_Siswa;
+    }
+
+    /**
+     * @return the Nama_Siswa
+     */
+    public String getNama_Siswa() {
+        return Nama_Siswa;
+    }
+
+    /**
+     * @return the Tgl_Lahir
+     */
+    public String getTgl_Lahir() {
+        return Tgl_Lahir;
+    }
+
+    /**
+     * @return the Asal_Sekolah
+     */
+    public String getAsal_Sekolah() {
+        return Asal_Sekolah;
+    }
+
+    /**
+     * @return the Jenis_Kelamin
+     */
+    public String getJenis_Kelamin() {
+        return Jenis_Kelamin;
+    }
+
+    /**
+     * @return the Kota_Asal
+     */
+    public String getKota_Asal() {
+        return Kota_Asal;
+    }
+
+    /**
+     * @return the Email_Siswa
+     */
+    public String getEmail_Siswa() {
+        return Email_Siswa;
+    }
+
+    /**
+     * @return the ID_Wali
+     */
+    public int getID_Wali() {
+        return ID_Wali;
+    }
+
+    /**
+     * @return the Nama_Wali
+     */
+    public String getNama_Wali() {
+        return Nama_Wali;
+    }
+
+    /**
+     * @return the ID_Prodi
+     */
+    public int getID_Prodi() {
+        return ID_Prodi;
+    }
+
+    /**
+     * @return the Nama_Prodi
+     */
+    public String getNama_Prodi() {
+        return Nama_Prodi;
+    }
+}
